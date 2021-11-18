a95f184a1fb9260292a1ca7b0b67b0b1de27f277
seyitErdemir <72307285+seyitErdemir@users.noreply.github.com>
2021-11-18 23:58:01 +0300

    23.50

--- Kurs/ES6,Promise,Fetch/Fetch/app.js
+++ Kurs/ES6,Promise,Fetch/Fetch/app.js
@@ -1,56 +1,8 @@
- function getData(data){
-    return new Promise(function(resolve,reject){
-
-      setTimeout(function(){
-
-        if (typeof data==="string") {
-            //olumlu
-            resolve(data)
-        }else  {
-            //olumsuz
-            reject(new Error("lütfen string bir deger giriniz"))
-        }
-
-      },5000)  
-      
-
-    })
- }
-
-
-
-//  getData(5).then(function(response){
-//      console.log("gelen deger : "+ response)
-//  }).catch(function(err){
-//     console.error(err)
-//  })
- 
-// getData(24)
-// .then(response => console.log("gelen deger : "+ response) )
-// .catch( err => console.error(err) )
-
-
-function addTwo(number){
-
-    return new Promise( (resolve, reject) =>{
-        setTimeout(function(){
-            if (typeof number ==="number") {
-                resolve(number+2)
-            }else{
-            reject(new Error("lütfen bir sayi giriniz"))
-                
-            }
-        },2000)
-    })
-
+function getTextFile(){
+    fetch("example.txt")
+    .then(response => response.text())
+    .then(data => console.log(data))
+    .catch( err => console.log(err))
 }
 
-addTwo("seyit").then(response =>{
-    console.log(response) 
-    return response+2
-}).then(response2=>     console.log(response2) )
-.catch(err =>  console.error(err))
-
-
-//1 tane catch bşrden cok then kullanabiliri
- 
+getTextFile();
