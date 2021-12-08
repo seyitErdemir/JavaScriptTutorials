bd95138b8febf1dd356780871c7de2dc55de1a0b
seyitErdemir <72307285+seyitErdemir@users.noreply.github.com>
2021-12-08 19:14:38 +0300

    .

---
+++ Node.js/Paket Oluşturma/index.js
@@ -0,0 +1,14 @@
+console.log("bu benim uygulama")
+
+// indirdigim paketlerin klasoru olan node u silerse sonrasında npm install yaparak ihtiyacı olan paketleri indirmesini saglıyabilirim
+//bu paketleri package.json daki dependencies nesnesinden almakta 
+// npm i paketadi --save-dev yaparakda  devDependencies paketlerini kaydedebilir tutabilirim
+
+
+
+//elinizdeki dosyalarını npm de paket halinde paylaşmak için
+//npm login 
+//username pasword
+//email adresi
+//npm publish  yayınlamak için
+//npm upublish paketadi --force     yayınlanmış paketi kaldrımak için
