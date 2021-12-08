f788e95f1368178cb92d17e787ebe4fbf56dda0e
seyitErdemir <72307285+seyitErdemir@users.noreply.github.com>
2021-12-08 19:50:43 +0300

    19.50

---
+++ Node.js/Babel Kullanimi/src/test1.js
@@ -0,0 +1,10 @@
+//es6
+
+class Person{
+    static test(){
+        console.log("test")
+    }
+}
+
+//npm install  ile gerekli paketleri yükle
+//npm run build     package.json dosyamdaki  scripts objesinin build isimli değerini çalıştır
---
+++ Node.js/Babel Kullanimi/src/test2.js
@@ -0,0 +1,5 @@
+async function test() {
+        return"merhaba"
+}
+
+test()
