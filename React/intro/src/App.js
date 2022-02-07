46a178a2453679ad78518a43a901be703c400698
seyitErdemir <72307285+seyitErdemir@users.noreply.github.com>
2022-02-07 11:51:16 +0300

    first companent

---
+++ React/intro/src/App.js
@@ -0,0 +1,15 @@
+import CategoryList from './CategoryList'
+import Navi from './Navi'
+import ProductList from './ProductList'
+
+function App () {
+  return (
+    <div>
+      <Navi></Navi>
+      <CategoryList></CategoryList>
+      <ProductList></ProductList>
+    </div>
+  )
+}
+
+export default App
---
+++ React/intro/src/Navi.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class Navi extends Component {
+  render () {
+    return <div>
+        <h2>Navi companent</h2>
+    </div>
+  }
+}
