e063164c0a931eb2d61fd35a48d81bc21865567a
seyitErdemir <72307285+seyitErdemir@users.noreply.github.com>
2022-02-11 17:53:23 +0300

    17.50

---
+++ React/redux-intro/src/redux/reducers/counterReducer.js
@@ -0,0 +1,20 @@
+import * as actionTypes from '../actions/actionTypes'
+
+const counterReducer = (state = 0, action) => {
+  let newState
+  switch (action.type) {
+    case actionTypes.INCREASE_COUNTER:
+      return (newState = state + action.payload)
+
+    case actionTypes.DECREASE_COUNTER:
+      return (newState = state - action.payload)
+
+    case actionTypes.INCREASE_BY_TWO_COUNTER:
+      return (newState = state + action.payload)
+      break
+    default:
+      return state
+  }
+}
+
+export default counterReducer 
