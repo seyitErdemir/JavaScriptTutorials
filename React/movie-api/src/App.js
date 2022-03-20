fed8a6cc09bd5a5fcb78fe781c480d2b875ea0d5
seyitErdemir <72307285+seyitErdemir@users.noreply.github.com>
2022-03-20 22:54:39 +0300

    .

---
+++ React/movie-api/src/App.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <div className='container    mx-auto '>
+        <div className='  grid-rows-1   px-4 border-4 h-16 border-indigo-600 mt-6'>
+          SADASDsad
+        </div>
+        <div className=' grid grid-cols-2 gap-4       px-4 border-4  p-8  border-indigo-600 mt-6 '>
+          <div className='     4 border-4 h-16 border-indigo-600  '></div>
+          <div className='      4 border-4 h-16 border-indigo-600   '></div>
+        </div>
+
+        <div className=' grid grid-cols-4  gap-4       px-4 border-4  p-8  border-indigo-600 mt-6 '>
+          <div className=' col-span-2    4 border-4 h-16 border-indigo-600  '></div>
+          <div className='      4 border-4 h-16 border-indigo-600   '></div>
+          <div className='      4 border-4 h-16 border-indigo-600   '></div>
+     
+        </div>
+
+        
+        <div className=' grid grid-cols-12  gap-6       px-4 border-4  p-8  border-indigo-600 mt-6 '>
+          <div className=' col-span-2    4 border-4 h-16 border-indigo-600  '></div>
+          <div className='      4 border-4 h-16 border-indigo-600   '></div>
+          <div className='      4 border-4 h-16 border-indigo-600   '></div>
+          <div className='      4 border-4 h-16 border-indigo-600   '></div>
+          <div className='      4 border-4 h-16 border-indigo-600   '></div>
+          <div className=' col-span-2    4 border-4 h-16 border-indigo-600  '></div>
+
+     
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
