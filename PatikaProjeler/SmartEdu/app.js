9385383894073caeed2c81cad2ac729fab26348a
seyitErdemir <72307285+seyitErdemir@users.noreply.github.com>
2022-04-03 06:27:58 +0300

    finish

--- PatikaProjeler/SmartEdu/app.js
+++ PatikaProjeler/SmartEdu/app.js
@@ -30,7 +30,6 @@ app.set('view engine', 'ejs')
 global.userIN = null
 
 //Middlewares
-
 app.use(express.static('public'))
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
