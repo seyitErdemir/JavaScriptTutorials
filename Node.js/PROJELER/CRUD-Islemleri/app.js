a38bca7205b4749f49274974036eafc33341b500
Seyit Erdemir <seyiterdemir4242@gmail.com>
2022-10-13 23:48:13 +0300

    .

--- Node.js/PROJELER/CRUD-Islemleri/app.js
+++ Node.js/PROJELER/CRUD-Islemleri/app.js
@@ -1,23 +1,79 @@
-const express = require('express');
-
-// Constants
-const PORT = 3000;
-const HOST = '0.0.0.0';
-
-// App
+const express = require("express");
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello Worldsss');
-});
+const port = 3000;
 
-app.get('/:id', (req, res) => {
-  res.send('Hello Worldsss' 
-  + req.params.id);
-});
+app.get("/", (req, res) => {
+  try {
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+    let url = 'http://localhost:3001/products'
 
+
+
+      function getProducts(url) {
+      console.log(url);
+ 
+      let response =   fetch(url);
+
+      if (response.ok) { // if HTTP-status is 200-299
+        // get the response body (the method explained below)
+        let json =   response.json();
+
+        res.send(
+          json
+        )
+
+      } else {
+        alert("HTTP-Error: " + response.status);
+      }
+
+
+    }
+
+    
+    
+    return fetch(url)
+      .then(response => response.json())
+      .then(result => result)
+  
+
+   
+  } catch (error) {
+    res.send(
+      "Hata Mesajıs"
+    ) 
+  }
+}
+
+
+  
+);
+
+app.get("/:id", (req, res) =>
+  res.send(
+    req.params.id
+  )
+);
+
+app.listen(port, () =>
+  console.log(`Example app listensdsing on port ${port}!`)
+);
+
+// const express = require('express');
+
+// // Constants
+// const PORT = 3000;
+// const HOST = '0.0.0.0';
+
+// // App
+// const app = express();
+
+// app.get('/', (req, res) => res.send('Node.js local development on Docker container and Nodemon work!'))
+
+// app.get('/:id', (req, res) => {
+//     res.send('Hello Worldsss' + req.params.id);
+// });
+
+// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 // const express = require('express')
 // const mainRoute = require('./routes/mainRoute')
@@ -30,9 +86,6 @@ console.log(`Running on http://${HOST}:${PORT}`);
 // app.use(express.static('public'))
 // app.use('/', mainRoute)
 
-
 // app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`)
+// console.log(`Example app listening on port ${port}`)
 // })
-
-
